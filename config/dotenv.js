f0af0fd99db7fd098fd91608254fdd3fe459bf2f
Herlan Mustopa <hrmustopa.dev@gmail.com>
2025-02-12 11:08:43 +0700

    update and getId

--- config/dotenv.js
+++ config/dotenv.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 5000,
-//   firebaseApiKey: process.env.FIREBASE_API_KEY,
-//   firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
   googleAppCredential: process.env.FIREBASE_CREDENTIALS,
 
 };
